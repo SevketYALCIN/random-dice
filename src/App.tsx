4592d420a32131f4c9e0c6f84cacbd7cde1617a8
SevketYALCIN <sevket.yalcin@outlook.com>
2020-08-03 16:40:18 +0200

    mode menu + head or tails feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // 0 for head or tail mode, 1 for options mode
+  const [mode, setMode] = useState<null | number>(null)
+
+  // Head or tails mode
+  const [state, setState] = useState<null | number>(null)
+
+  // Option mode
   const [max, setMax] = useState<number>(0)
-  const [random, setRandom] = useState<null|number>(null)
+  const [random, setRandom] = useState<null | number>(null)
   const [newValue, setNewValue] = useState('')
   const [values, setValues] = useState<Array<String>>([])
 
@@ -19,7 +26,7 @@ function App() {
   }
 
   let getRand = () => {
-    if(values.length > 0) {
+    if (values.length > 0) {
       setRandom(null)
       setTimeout(() => {
         setRandom(rand(max))
@@ -31,23 +38,50 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="container">
-          {values.map((v, i) => <p className={random === i ? 'active' : ''}>{v}</p>)}
-          <input
-            placeholder="Type a value and press enter"
-            value={newValue}
-            onChange={(e) => setNewValue(e.currentTarget.value)}
-            onKeyPress={newValueKeyPressed} />
-          <div 
-            className={`button ${values.length > 0 ? '' : 'disabled'}`} 
-            onClick={() => getRand()}>
-              Get a random option
+          {mode === null &&
+            <div>
+              <div className="button" onClick={() => setMode(0)}>Head or Tails</div>
+              <div className="button" onClick={() => setMode(1)}>Random option</div>
             </div>
+          }
+          {mode === 0 &&
+            <div className="headortail">
+              {state !== null && <div>{state === 0 ? "Head" : "Tails"}</div>}
+              <div
+                className="button"
+                onClick={() => setState(Math.floor(Math.random() * 2))}>
+                Click to get head or tails
+              </div>
+            </div>
+          }
+          {mode === 1 &&
+            <div className="options">
+              {values.map((v, i) => <p className={random === i ? 'active' : ''}>{v}</p>)}
+              <input
+                placeholder="Type a value and press enter"
+                value={newValue}
+                onChange={(e) => setNewValue(e.currentTarget.value)}
+                onKeyPress={newValueKeyPressed} />
+              <div
+                className={`button ${values.length > 0 ? '' : 'disabled'}`}
+                onClick={() => getRand()}>
+                Get a random option
+            </div>
+            </div>
+          }
+          {mode !== null && <button onClick={() => {
+            setMode(null)
+            setState(null)
+            setRandom(null)
+            setNewValue('')
+            setValues([])
+          }}>Back to home</button>}
         </div>
       </header>
     </div>
   );
 }
 
-let rand = (max:number) => Math.floor(Math.random() * max)
+let rand = (max: number) => Math.floor(Math.random() * max)
 
 export default App;
